d8fb9925e4938ac2415f486a6b7f0f9b7e129e3f
gbroccol <gbroccol@student.21-school.ru>
2021-01-04 10:46:46 +0300

    day 00

---
+++ day00/day00/ex01/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <iomanip>
+#include "person.hpp"
+
+int main()
+{
+	Person Nastya;
+
+	Nastya.add_person();
+	Nastya.search_short();
+	Nastya.search_full();
+
+	return (0);
+}
