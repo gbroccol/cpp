4ae14f73c86cbfe0344c6c7ba1b7fe7fbca33b3f
Anastasiq Pchelina <gbroccol@mi-i5.msk.21-school.ru>
2021-01-13 18:20:43 +0300

    new

--- day01/ex02/Zombie.hpp
+++ day01/ex02/Zombie.hpp
@@ -1,27 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ZombieEvent.hpp                                    :+:      :+:    :+:   */
+/*    Zombie.hpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: gbroccol <gbroccol@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/01/13 12:19:10 by gbroccol          #+#    #+#             */
-/*   Updated: 2021/01/13 14:20:48 by gbroccol         ###   ########.fr       */
+/*   Created: 2021/01/13 12:18:55 by gbroccol          #+#    #+#             */
+/*   Updated: 2021/01/13 12:20:33 by gbroccol         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef ZOMBIEEVENT_HPP
-# define ZOMBIEEVENT_HPP
+#ifndef ZOMBIE_HPP
+# define ZOMBIE_HPP
 
-# include "Zombie.hpp"
+// #include <iostream>
+// #include <string>
 
-class ZombieEvent
+class  Zombie
 {
 private:
 	
 public:
-	ZombieEvent();
-	~ZombieEvent();
+	 Zombie();
+	~ Zombie();
 };
 
 #endif
