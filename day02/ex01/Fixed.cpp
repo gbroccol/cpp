1a97cf3096a27fa5ccf9ae6f81f8881f064bb2d6
Anastasiq Pchelina <gbroccol@oa-f5.msk.21-school.ru>
2021-01-19 15:21:37 +0300

    OK

--- day02/ex01/Fixed.cpp
+++ day02/ex01/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: gbroccol <gbroccol@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/18 17:34:20 by gbroccol          #+#    #+#             */
-/*   Updated: 2021/01/18 20:03:11 by gbroccol         ###   ########.fr       */
+/*   Updated: 2021/01/19 15:21:19 by gbroccol         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,20 +20,20 @@ Fixed::Fixed(void) : value(0)
 	return ;
 }
 
-Fixed::Fixed(int int_value) : value(int_value << this->bits)
+Fixed::Fixed(int int_value) : value(int_value << this->bits)						// convert int to int fixed point
 {
 	std::cout << "\x1b[31mInt constructor called\x1b[0m" << std::endl;
 }
 
-Fixed::Fixed(float float_value) : value(roundf(float_value * (1 << this->bits))) 	// 2^8 = 256
+Fixed::Fixed(float float_value) : value(roundf(float_value * (1 << this->bits))) 	// convert from floating-point to fixed-point
 {
 	std::cout << "\x1b[31mFloat constructor called\x1b[0m" << std::endl;
 }
 
-Fixed::Fixed(Fixed const &new_fixed)
+Fixed::Fixed(Fixed const &new_fixed)												// copy class to this
 {
 	std::cout << "\x1b[31mCopy constructor called\x1b[0m" << std::endl;
-	*this = new_fixed; 																// скопировать значения в this
+	*this = new_fixed; 				
 	return ;
 }
 
@@ -68,7 +68,7 @@ int				Fixed::toInt(void) const
 
 float			Fixed::toFloat(void) const
 {
-	return (this->value / roundf(1 << this->bits));					// 2^8 = 256 
+	return (this->value / roundf(1 << this->bits));					// 
 }
 
 std::ostream & operator<<(std::ostream & o, Fixed const & src)		// в какой поток записывать // с каким классом работать
